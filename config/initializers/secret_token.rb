3ccc3f6b477cc823bfabb729c180e2f3fdfab7df
Eric Kidd <git@randomhacks.net>
2012-08-01 13:44:22 -0400

    Remove secret token from version control
    This has been regenerated and is now stored in an environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Langforums::Application.config.secret_token = '2f984e442f2049f87fc735387442a3e4c512cd8ab226ce578677e02ee6a0e4ffb78c623f8a47a25a296b6037959d60f603a7a300f3b6ee02cb6c21ce5c4944b9'
+Langforums::Application.config.secret_token = ENV['SECRET_TOKEN']
