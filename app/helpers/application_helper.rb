7a7ec15e4a1b8754372f629ff2b15d37be6b7b2a
Eric Kidd <git@randomhacks.net>
2012-08-03 09:20:35 -0400

    Link to GitHub

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def htlal_forum_url
     'http://how-to-learn-any-language.com/forum/default.asp'
   end
+
+  # The source code for this site.
+  def langforums_github_url
+    'https://github.com/emk/langforums'
+  end
 end
